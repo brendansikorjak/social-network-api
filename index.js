79bb86034929cb78b7f0fa45785ea8d7968a5360
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-15 14:47:29 -0400

    added index.js and connection.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
