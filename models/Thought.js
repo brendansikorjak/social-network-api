ee35d36494383fa8878d1e5d9664e01a0c330253
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-18 17:55:46 -0400

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
